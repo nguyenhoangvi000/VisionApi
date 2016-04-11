20c39342a8f7bc228431535eb38dac6605ddfe84
Nguyễn Hoàng Vĩ <nguyenhoangvi000@gmail.com>
2016-04-11 11:20:37 +0700

    All

---
+++ VisionAPI/FaceDetectApp.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using Google.Apis;
+using Google.Apis.Requests;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Vision.v1;
+using Google.Apis.Vision.v1.Data;
+
+namespace VisionAPI
+{
+    public class FaceDetectApp
+    {
+
+        private VisionService _vision;
+        
+
+        public FaceDetectApp(VisionService vision)
+        {
+            this._vision = vision;
+        }
+        public IList<FaceAnnotation> detectFaces(string path, int maxResults)
+        {
+            byte[] data = File.ReadAllBytes(path);
+
+            AnnotateImageRequest request = new AnnotateImageRequest();
+            Google.Apis.Vision.v1.Data.Image img = new Google.Apis.Vision.v1.Data.Image();
+            img.Content = Convert.ToBase64String(data);
+            request.Image = img;
+
+            Feature feature = new Feature();
+            feature.Type = "FACE_DETECTION";
+            feature.MaxResults = maxResults;
+
+            request.Features = new List<Feature>()
+            {
+                feature
+            };
+            
+
+
+            BatchAnnotateImagesRequest batchAnnotate = new BatchAnnotateImagesRequest();
+            batchAnnotate.Requests = new List<AnnotateImageRequest>() {
+                request
+            };
+            ImagesResource.AnnotateRequest annotate = _vision.Images.Annotate(batchAnnotate);
+
+            BatchAnnotateImagesResponse batchResponse = annotate.Execute();
+
+            AnnotateImageResponse response = batchResponse.Responses[0];
+
+            if (response.FaceAnnotations == null)
+            {
+                throw new Exception(response.Error.Message);
+            }
+
+            return response.FaceAnnotations;
+
+        }
+
+        
+
+        public void annotateWithFaces(Bitmap bmp, IList<FaceAnnotation> faces)
+        {
+            foreach (FaceAnnotation face in faces)
+            {
+                annotateWithFace(bmp, face);
+            }
+            
+        }
+
+
+
+        private void annotateWithFace(Bitmap bmp, FaceAnnotation face)
+        {
+            Graphics graphics = Graphics.FromImage(bmp);
+            List<Point> point = new List<Point>();
+
+            foreach (var item in face.BoundingPoly.Vertices)
+            {
+                
+                point.Add(new Point(item.X.Value, item.Y.Value));
+
+            }
+
+            point.Add(point[0]);
+            graphics.DrawLines(Pens.Aqua, point.ToArray());
+
+            graphics.Dispose();
+        }
+
+
+       
+
+    }
+}
---
+++ VisionAPI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using VisionAPI;
+using Google.Apis.Vision.v1;
+using Google.Apis.Services;
+using Google.Apis.Vision.v1.Data;
+
+namespace SmartHome
+{
+    public static class Program
+    {
+        private static string Application_Name = "Face Detect";
+        private static int MAX_RESULTS = 20;
+
+        public static void Main()
+        {
+            FaceDetectApp app = new FaceDetectApp(CreateVisionServiceClient());
+
+            var faces = app.detectFaces("E:\\2.jpg", MAX_RESULTS);
+
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap("E:\\2.jpg");
+
+            app.annotateWithFaces(bmp, faces);
+            bmp.Save("E:\\21.jpg");
+        }
+
+        public static VisionService CreateVisionServiceClient()
+        {
+            using (StreamReader sr=new StreamReader("E:\\app.json"))
+            {
+                var credentials = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(sr.BaseStream);
+
+
+                if (credentials.IsCreateScopedRequired)
+                {
+                    credentials = credentials.CreateScoped(new[] { VisionService.Scope.CloudPlatform });
+                }
+
+                var serviceInitializer = new BaseClientService.Initializer()
+                {
+                    ApplicationName = Application_Name,
+                    HttpClientInitializer = credentials
+                };
+                return new VisionService(serviceInitializer);
+            }
+        }
+    }
+}
